63ac19ca381918b3492e77d3a74d64b0a82a65c2
Marcin Domański <marcindomanski.pl@gmail.com>
2016-02-29 00:27:23 +0100

    api_view decorator working

--- shoppingListServer/urls.py
+++ shoppingListServer/urls.py
@@ -18,12 +18,8 @@ from rest_framework import routers
 from django.contrib import admin
 from listService import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^shopping_list/$', views.element_list)
 ]
